54499f352725e3a256a230cc8c222de55580e56f
Esmée Zootjes <esmeezootjes@gmail.com>
2023-02-13 22:42:55 +0100

    eerste elementen gemaakt, kleine aanpassingen homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import HelloWorld from "./HelloWorld";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <main>
+          <HelloWorld />
+        </main>
+        <footer>Coded by Esmée Zootjes</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
