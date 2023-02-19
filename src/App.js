ba3bcca841593feecb949e120a56ef08ecaffafe
Esmée Zootjes <esmeezootjes@gmail.com>
2023-02-19 19:16:57 +0100

    fonts added

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header"></header>
         <main>
           <HelloWorld />
         </main>
